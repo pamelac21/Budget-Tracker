72ca6130db324e4b9db596401d39cf26c45da111
PamelaCleveland <pamelac021@gmail.com>
2021-12-06 23:05:40 -0500

    final

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,70 +1,68 @@
 let db;
-const request = indexedDB.open('budget_tracker', 1);
+const request = indexedDB.open("budget_tracker", 1);
 
-request.onupgradeneeded = function(event) {
-    const db = event.target.result;
-    db.createObjectStore('new_transaction', { autoIncrement: true });
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
-request.onsuccess = function(event) {
-    db = event.target.result;
+request.onsuccess = function (event) {
+  db = event.target.result;
   // check if app is online
-    if (navigator.onLine) {
-        uploadTransaction();
-    }
+  if (navigator.onLine) {
+    uploadTransaction();
+  }
 };
 
-request.onerror = function(event) {
-    console.log(event.target.errorCode);
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
-    const  budgetObjectStore = transaction.objectStore('new_transaction');
-    budgetObjectStore.add(record);
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+  budgetObjectStore.add(record);
 }
 
 function uploadTransaction() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["new_transaction"], "readwrite");
 
-    // open a transaction on your pending db
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  const budgetObjectStore = transaction.objectStore("new_transaction");
 
-    const budgetObjectStore = transaction.objectStore('new_transaction');
+  const getAll = budgetObjectStore.getAll();
 
-    const getAll = budgetObjectStore.getAll();
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
 
-    getAll.onsuccess = function() {
+          const transaction = db.transaction(["new_transaction"], "readwrite");
 
-        // if there was data in indexedDb's store, send it to the api server
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(response => response.json())
-                .then(serverResponse => {
-                    if (serverResponse.message) {
-                        throw new Error(serverResponse);
-                    }
+          const budgetObjectStore = transaction.objectStore("new_transaction");
 
-                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+          budgetObjectStore.clear();
 
-                    const budgetObjectStore = transaction.objectStore('new_transaction');
-
-                    budgetObjectStore.clear();
-
-                    alert('All saved transactions have been submitted!');
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
+          alert("All saved transactions have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  };
 }
 
 // listen for app coming back online
-window.addEventListener('online', uploadTransaction);
+window.addEventListener("online", uploadTransaction);

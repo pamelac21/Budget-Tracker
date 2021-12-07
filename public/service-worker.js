5a58f18588f5e0f340ae41a88e11fd88696dd76b
PamelaCleveland <pamelac021@gmail.com>
2021-12-06 23:14:40 -0500

    added iconsto cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,15 @@ const FILES_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
     "./js/index.js",
-    "./js/idb.js"
+    "./js/idb.js",
+    './icons/icon-512x512.png',
+    './icons/icon-384x384.png',
+    './icons/icon-192x192.png',
+    './icons/icon-152x152.png',
+    './icons/icon-144x144.png',
+    './icons/icon-128x128.png',
+    './icons/icon-96x96.png',
+    './icons/icon-72x72.png'
 
 ];
 
